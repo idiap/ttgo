6566c516f7f3fe3add9ef74f344d8709d2807eb4
Suhan Shetty <suhan.n.shetty@gmail.com>
2023-02-27 10:40:06 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 '''
     Copyright (c) 2008 Idiap Research Institute, http://www.idiap.ch/
-    Written by Suhan Shetty <suhan.shetty@idiap.ch>,
+    Written by Suhan Shetty <suhan.shetty@idiap.ch>
 '''
 
 import torch 
