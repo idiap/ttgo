978ab4d958c279e6e9eda3a5c05e82252c858453
Suhan SHETTY <sshetty@vgni012.idiap.ch>
2023-03-30 15:09:52 +0200

    added tt_vs_nn

--- tt_utils.py
+++ tt_utils.py
@@ -1,3 +1,23 @@
+'''
+    Copyright (c) 2022 Idiap Research Institute, http://www.idiap.ch/
+    Written by Suhan Shetty <suhan.shetty@idiap.ch>,
+   
+    This file is part of TTGO.
+
+    TTGO is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License version 3 as
+    published by the Free Software Foundation.
+
+    TTGO is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with TTGO. If not, see <http://www.gnu.org/licenses/>.
+'''
+
+
 import torch
 import tntorch as tnt
 import numpy as np
